d8affc128b19f7b6f949ed7b67916bdb9dfe36fb
Gianca <giancade8@gmail.com>
2021-01-12 16:34:57 +0100

    Aggiunta gestione personalizzata su costrutture EF

--- Models/Services/Application/CourseService.cs
+++ Models/Services/Application/CourseService.cs
@@ -2,29 +2,29 @@ using System;
 using System.Collections.Generic;
 using MyCourse.Models.Enums;
 using MyCourse.Models.ValueTypes;
-using MyCourse.Models.ViewModels;   
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Models.Services.Application
 {
-    public class CourseService : ICourseService
+    public class CourseService //: ICourseService
     {
         public List<CourseViewModel> GetCourses()
         {
             var courseList = new List<CourseViewModel>();
             var rand = new Random();
-            for (int i =1; i <=20; i++)
+            for (int i = 1; i <= 20; i++)
             {
-                var price = Convert.ToDecimal(rand.NextDouble() * 10 +10);
+                var price = Convert.ToDecimal(rand.NextDouble() * 10 + 10);
                 var course = new CourseViewModel
                 {
                     Id = i,
                     Title = $"Corso {i}",
                     CurrentPrice = new Money(Currency.EUR, price),
-                    FullPrice = new Money(Currency.EUR, rand.NextDouble()>0.5? price:price-1),
-                    Author = "Nome Cognome",
-                    Rating = rand.NextDouble()*5.0,
-                    ImagePath="/logo.svg"
-                } ;
+                    FullPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price + 1),
+                    Author = "Nome cognome",
+                    Rating = rand.Next(10, 50) / 10.0,
+                    ImagePath = "/logo.svg"
+                };
                 courseList.Add(course);
             }
             return courseList;
--- Models/Services/Application/ICourseService.cs
+++ Models/Services/Application/ICourseService.cs
@@ -7,7 +7,6 @@ namespace MyCourse.Models.Services.Application
     public interface ICourseService
     {
          Task<List<CourseViewModel>> GetCoursesAsync();
-
          Task<CourseDetailViewModel> GetCourseAsync(int id);
     }
 }
