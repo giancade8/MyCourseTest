952d59c6372dbc7f299fb9b25a0011317deb8f3c
Gianca <giancade8@gmail.com>
2021-01-13 11:21:51 +0100

    Aggiunto db context EF per lista corsi

--- Models/Entities/Lessons.cs
+++ Models/Entities/Lessons.cs
@@ -6,7 +6,7 @@ namespace MyCourse.Models.Entities
     public partial class Lesson
     {
         public long Id { get; set; }
-        public long CourseId { get; set; }
+        public int CourseId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Duration { get; set; }
