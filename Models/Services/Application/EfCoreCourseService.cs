f3ae39480df8862c41371617b9660c1b6960bcdf
Gianca <giancade8@gmail.com>
2021-01-13 11:43:38 +0100

    Aggiunto detail courses EF

--- Models/Services/Application/EfCoreCourseService.cs
+++ Models/Services/Application/EfCoreCourseService.cs
@@ -16,9 +16,33 @@ namespace MyCourse.Models.Services.Application
             this.dbContext = dbContext;
 
         }
-        public Task<CourseDetailViewModel> GetCourseAsync(int id)
+        public async Task<CourseDetailViewModel> GetCourseAsync(int id)
         {
-            throw new System.NotImplementedException();
+            CourseDetailViewModel viewModel = await dbContext.Courses
+                .Where(course => course.Id == id)
+                .Select(course =>
+                new CourseDetailViewModel {
+                    Id = course.Id,
+                    Title = course.Title,
+                    Description = course.Description,
+                    ImagePath = course.ImagePath,
+                    Author = course.Author,
+                    Rating = course.Rating,
+                    CurrentPrice = course.CurrentPrice,
+                    FullPrice = course.FullPrice,
+                    Lessons = course.Lessons.Select(lesson => new LessonViewModel 
+                    {
+                        Id = lesson.Id,
+                        Title = lesson.Title,
+                        Description = lesson.Description,
+                        Duration = lesson.Duration
+                    }).ToList()
+                })
+                .SingleAsync(); //Restituisce il primo elemento dell'elenco. in caso di 0 o +1 solleva eccezione
+                //.FirstAsync() -- restitusce sempre il primo elemento, ma da errore se elenco vuoto
+                //.SingleOrDefaultAsync tollera che l'elenco sia vuoto, ma non piu elementi di 1.
+                //.FirstOrDefaultAsync non solleva mai eccezione, restituisce il primo se c'è altriment null se è vuoto    
+            return viewModel;
         }
 
         public async Task<List<CourseViewModel>> GetCoursesAsync()
